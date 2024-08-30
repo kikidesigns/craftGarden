27d6ff252941e637cd71f4dd3c3dd7380e18664b
Maria Garcia <79329688+kikidesigns@users.noreply.github.com>
2024-08-30 15:31:23 -0500

    Refactor App.tsx: Remove HotBar component, update LandingPage content.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,6 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 import TarotExperience from './TarotExperience'
 
-function HotBar() {
-  return (
-    <div className="hot-bar">
-      <Link to="/">Home</Link>
-      <Link to="/tarot">Tarot</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </div>
-  )
-}
-
 function LandingPage() {
   return (
     <div className="galaxy">
@@ -34,24 +23,9 @@ function App() {
       <div className="app-container">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/tarot" element={
-            <>
-              <HotBar />
-              <TarotExperience />
-            </>
-          } />
-          <Route path="/about" element={
-            <>
-              <HotBar />
-              <div>About Page</div>
-            </>
-          } />
-          <Route path="/contact" element={
-            <>
-              <HotBar />
-              <div>Contact Page</div>
-            </>
-          } />
+          <Route path="/tarot" element={<TarotExperience />} />
+          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="/contact" element={<div>Contact Page</div>} />
         </Routes>
       </div>
     </Router>
